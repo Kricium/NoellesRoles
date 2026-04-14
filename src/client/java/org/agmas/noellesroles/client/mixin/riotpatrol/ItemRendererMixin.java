bd541d1b9dabcdcf891333e4285d6fec142d36a4
yinfires <2199153624@qq.com>
2026-04-14 11:28:50 +0800

    修复防暴叉物品栏与手持模型渲染

--- src/client/java/org/agmas/noellesroles/client/mixin/riotpatrol/ItemRendererMixin.java
+++ src/client/java/org/agmas/noellesroles/client/mixin/riotpatrol/ItemRendererMixin.java
@@ -4,59 +4,16 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.fabricmc.fabric.api.client.model.loading.v1.FabricBakedModelManager;
 import org.agmas.noellesroles.ModItems;
 import org.agmas.noellesroles.client.NoellesrolesClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
-
-    @Redirect(
-        method = "getModel",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z",
-            ordinal = 0
-        )
-    )
-    private boolean noellesroles$allowRiotForkHandModel(ItemStack stack, Item item) {
-        return isRiotForkOrOriginal(stack, item);
-    }
-
-    @Redirect(
-        method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z",
-            ordinal = 0
-        )
-    )
-    private boolean noellesroles$allowRiotForkInventoryModel(ItemStack stack, Item item) {
-        return isRiotForkOrOriginal(stack, item);
-    }
-
-    @Redirect(
-        method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z",
-            ordinal = 2
-        )
-    )
-    private boolean noellesroles$allowRiotForkBuiltinPath(ItemStack stack, Item item) {
-        return isRiotForkOrOriginal(stack, item);
-    }
-
-    private static boolean isRiotForkOrOriginal(ItemStack stack, Item item) {
-        return stack.isOf(item) || (item == Items.TRIDENT && stack.isOf(ModItems.RIOT_FORK));
-    }
 
     @ModifyVariable(
         method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
@@ -70,6 +27,8 @@ public class ItemRendererMixin {
         ModelTransformationMode renderMode,
         boolean leftHanded
     ) {
+        // Keep the inventory sprite on the normal item model and only swap to the
+        // custom blockbench model while the fork is actually rendered in-hand.
         if (stack.isOf(ModItems.RIOT_FORK) && noellesroles$isHandRenderMode(renderMode)) {
             return ((FabricBakedModelManager) MinecraftClient.getInstance().getBakedModelManager()).getModel(NoellesrolesClient.RIOT_FORK_IN_HAND_MODEL_ID);
         }
